b8fed61be4e26bcb03cd9c4c199e29e48f33a982
xyfabcde <xyfabcde@126.com>
2016-11-23 15:08:49 +0800

    del uboot

--- u-boot/drivers/nand/driver/hw/transadaptor/dma_trans.c
+++
@@ -1,5 +0,0 @@
-/* dma trans function will be done later, now it's not be used, but only be compiled */
-/* in order to avoid be deleted when make distclean in kernel */
-#include <os_clib.h>
-#include <nand_debug.h>
-#include "transadaptor.h"
